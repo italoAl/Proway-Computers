18359d656a94f0dd3920d3ceb22bc8f2d29086d2
italoAl <italoalves20112010@hotmail.com>
2022-10-31 18:58:53 -0300

    Implementar funções.

--- src/app/carrinhos/carrinhos.component.ts
+++ src/app/carrinhos/carrinhos.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { CarrinhoService } from '../carrinho.service';
 import { IProdutoCarrinho } from '../produtos';
 
@@ -10,15 +11,30 @@ import { IProdutoCarrinho } from '../produtos';
 export class CarrinhosComponent implements OnInit {
 
   itensCarrinho: IProdutoCarrinho [] = [];
+  total = 0;
 
 
   constructor(
-    public carrinhoService: CarrinhoService
+    public carrinhoService: CarrinhoService,
+    private router: Router
   ) { }
 
   ngOnInit(): void {
     this.itensCarrinho = this.carrinhoService.obtemCarrinho();
+    this.calculaTotal();
   }
 
-
+  calculaTotal() {
+    this.total = this.itensCarrinho.reduce((prev, curr) => prev + (curr.preco * curr.Quantidade), 0);
+  }
+  removeProdutoCarrinho(produtoId: number) {
+    this.itensCarrinho = this.itensCarrinho.filter(item => item.id !== produtoId);
+    this.carrinhoService.removerProdutoCarrinho(produtoId);
+    this.calculaTotal();
+  }
+comprar() {
+  alert("Parabens você completou a sua compra")
+  this.carrinhoService.limparCarrinho();
+  this.router.navigate(["produtos"]);
+}
 }
